5bde2fbeb3d8fb60f55a77e2978520fabeb0afdd
Ayodélé BANSOU <ayodelebansou@MacBook-Air-de-Ayodele.local>
2024-09-27 12:19:47 +0200

    feat: reorganization project

--- FixCity-app/app/index.tsx
+++ FixCity-app/app/index.tsx
@@ -1,86 +1,36 @@
-import MapView from "react-native-map-clustering";
-import { Marker } from "react-native-maps";
-import React, { useState, useEffect } from 'react';
-import { Button, View, Alert, Dimensions, StyleSheet } from 'react-native';
+// import React, { useState, useEffect } from 'react';
 
-export default function Index() {
-  const [region, setRegion] = useState({
-    latitude: 48.85781676584989,
-    longitude: 2.2950763090818325,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  });
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useHistory } from 'react-router-native';
 
-  const [location, setLocation] = useState([
-    {
-      id : 1,
-      latitude: 48.85781676584989,
-      longitude: 2.2950763090818325,
-    },
-    {
-      id : 2,
-      latitude: 48.85181499513845, 
-      longitude:2.3054006502971864,
-    },
+const HelloWorld = () => {
+  const history = useHistory();
 
-  ]);
-  
-  const getLocation = async () => {
-    setRegion({
-      latitude : 48.85781676584989,
-      longitude : 2.2950763090818325,
-      latitudeDelta: 0.015,
-      longitudeDelta: 0.0121,
-    });
+  const handleButtonClick = () => {
+    history.push('/map/page.tsx');
   };
+
   return (
     <View style={styles.container}>
-
-      <MapView
-        style={{flex : 1}}
-        region={region}
-        onRegionChangeComplete={setRegion}
-      >
-        {location.map((marker) => (
-          <Marker
-          key={marker.id}
-          coordinate={{
-            latitude : marker.latitude,
-            longitude  : marker.longitude
-          }}
-          title="wsh les bggg"
-          description="c'est michou"
-        />
-        ))}
-        
-          {/* <Image
-            source={require("../assets/images/react-logo.png")}
-            style={{ width: 26, height: 28 }}
-            resizeMode="center"
-          /> */}
-
-      </MapView>
-      <View style={styles.buttonContainer}>
-        <Button title="Go to my location" onPress={getLocation} />
-      </View>
+      <Text style={styles.text}>Hello, World! Voilà c'est tout!</Text>
+      <Button title="Go to Other Page" onPress={handleButtonClick} />
     </View>
-    
-      
   );
-}
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0', // Couleur de fond optionnelle
   },
-  map: {
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
-  },
-  buttonContainer: {
-    position: 'absolute',
-    bottom: 20,
-    left: 20,
-    right: 20,
+  text: {
+    fontSize: 20,
+    color: '#000000', // Couleur du texte
   },
 });
 
+export default HelloWorld;
---
+++ FixCity-app/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HelloWorld = () => {
+    return <div>Welcome back jeune français !</div>;
+};
+
+export default HelloWorld;
+
+
+
+
